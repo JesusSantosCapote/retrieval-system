49d195e841985394ec929a47cfebc36a1ce294f5
sheldon05 <jesussantoscapote@gmail.com>
2022-11-10 14:09:23 -0500

    Add operations

--- boolean_expression_evaluator.py
+++ boolean_expression_evaluator.py
@@ -4,7 +4,15 @@ def query_tokenizer(query):
     """
     Returns the set of tokens. Tokens can be separated by any number of blanks
     """
-
     query = re.split(r'\s+', query)
     tokens = [token.lower() for token in query]
     return tokens
+
+
+# These lambda expressions map from operators to actual executable code
+operations = {
+    'and': lambda x,y: x and y,
+    'or': lambda x,y: x or y,
+    'not': lambda x: not x,
+}
+
