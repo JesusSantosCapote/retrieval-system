457cf93a14da6a0bdff03e7e383562d5e07194d8
dfg-98 <dariofg98@gmail.com>
2022-12-16 11:26:51 -0500

    feat: add django application

---
+++ retrieval_system/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from retrieval_system.users.models import User
+from retrieval_system.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
